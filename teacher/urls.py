bf013a94d28b7ef50fcef420c01b5ca83907ac5d
Rishabh <rishabhrathi000@gmail.com>
2020-08-31 09:51:10 +0530

    some changes

--- teacher/urls.py
+++ teacher/urls.py
@@ -6,6 +6,7 @@ from . import views
 teacher_view = views.TeacherViewSet.as_view({'get':'list'})
 teacher_create = views.TeacherViewSet.as_view({'post':'create'})
 teacher_login = views.TeacherViewSet.as_view({'post':'login'})
+teacher_logout = views.TeacherViewSet.as_view({'get':'logout'})
 teacher_detail = views.TeacherViewSet.as_view({'get': 'retrieve'})
 teacher_change_pwd = views.TeacherViewSet.as_view({'post':'change_password'})
 
@@ -14,6 +15,7 @@ urlpatterns = [
 	path('', teacher_view),
 	path('register/', teacher_create),
 	path('login/', teacher_login),
+	path('logout/', teacher_logout),
 	path('<str:name>/change_password', teacher_change_pwd),
 	# path('<str:name>/', teacher_detail)
 ]
